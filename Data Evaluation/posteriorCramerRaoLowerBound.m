d405f4edff9e1e7a4fd423a2a9804c845185a8b4
DBAA <hellocaoziyi@gmail.com>
2021-05-31 15:29:08 +0800

    修正了PCRLB的计算

--- Data Evaluation/posteriorCramerRaoLowerBound.m
+++ Data Evaluation/posteriorCramerRaoLowerBound.m
@@ -21,11 +21,13 @@ if mode == "KF"
         Z(2,:,iStation) = Z(2,:,iStation) - address(2,iStation);
         
         J(:,:,1,iStation) =inv(Q(:,:,1) + P(:,:,1,iStation))+H'*inv(Station.R(:,:,1,iStation))*H;
-        PCRBdata(1,iStation) = (trace(inv(J(:,:,1,iStation)))).^0.5;
+        PCRBtemp = inv(J(:,:,1,iStation));
+        PCRBdata(1,iStation) = (PCRBtemp(1,1) + PCRBtemp(3,3)).^0.5;
         
         for jIter = 2:nIter
             J(:,:,jIter,iStation) = inv(Q(:,:,jIter) + F*inv(J(:,:,jIter-1,iStation))*F')+H'*inv(Station.R(:,:,jIter,iStation))*H;
-            PCRBdata(jIter,iStation) = (trace(inv(J(:,:,jIter,iStation)))).^0.5;
+            PCRBtemp = inv(J(:,:,jIter,iStation));
+            PCRBdata(jIter,iStation) = (PCRBtemp(1,1) + PCRBtemp(3,3)).^0.5;
         end
     end
     
@@ -51,10 +53,12 @@ elseif mode == "EKF"
     PCRBdata = zeros(nIter,nStation);
     for iStation = 1:nStation
         J(:,:,1,iStation) =inv(Q(:,:,1) + P(:,:,1,iStation))++H(:,:,1,iStation)'*inv(Rpol(:,:,iStation))*H(:,:,1,iStation);
-        PCRBdata(1,iStation) = (trace(inv(J(:,:,1,iStation)))).^0.5;
+        PCRBtemp = inv(J(:,:,1,iStation));
+        PCRBdata(1,iStation) = (PCRBtemp(1,1) + PCRBtemp(3,3)).^0.5;
         for jIter = 2:nIter
             J(:,:,jIter,iStation) = inv(Q(:,:,jIter) + F*inv(J(:,:,jIter-1,iStation))*F')+H(:,:,jIter,iStation)'*inv(Rpol(:,:,iStation))*H(:,:,jIter,iStation);
-            PCRBdata(jIter,iStation) = (trace(inv(J(:,:,jIter,iStation)))).^0.5;
+            PCRBtemp = inv(J(:,:,jIter,iStation));
+            PCRBdata(jIter,iStation) = (PCRBtemp(1,1) + PCRBtemp(3,3)).^0.5;
         end
     end
 end
